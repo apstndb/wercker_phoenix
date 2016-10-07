2ec41af40c09f72d51e853bb0953bb3806c45d32
Shinta Hatatani <shinta.hatatani@aktsk.jp>
2016-10-07 15:54:41 +0900

    Use distillery

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule WerckerPhoenix.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:distillery, "~> 0.9"},
      {:cowboy, "~> 1.0"}]
   end
 
